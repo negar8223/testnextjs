f724b8018f15c7a06b7688ac3861caac37c1471d
Negar Rahmani <negar.negar8223@gmail.com>
2022-02-21 13:52:56 +0330

    Create dashboard

---
+++ pages/dashboard.tsx
@@ -0,0 +1,279 @@
+import {
+  ActionList,
+  AppProvider,
+  Card,
+  Frame,
+  Layout,
+  Navigation,
+  Page,
+  SkeletonBodyText,
+  SkeletonDisplayText,
+  SkeletonPage,
+  TextContainer,
+  TopBar,
+  ContextualSaveBar,
+  Loading,
+  Toast,
+  DescriptionList,
+} from "@shopify/polaris";
+import { useCallback, useRef, useState } from "react";
+
+import { useRouter } from "next/router";
+import {
+  ArrowLeftMinor,
+  HomeMajor,
+  SettingsMajor,
+} from "@shopify/polaris-icons";
+
+export default function FrameExample() {
+  const defaultState = useRef({
+    emailFieldValue: "dharma@jadedpixel.com",
+    nameFieldValue: "Jaded Pixel",
+  });
+  const { push } = useRouter();
+  const skipToContentRef = useRef(null);
+
+  const [toastActive, setToastActive] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isDirty, setIsDirty] = useState(false);
+  const [searchActive, setSearchActive] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const [userMenuActive, setUserMenuActive] = useState(false);
+  const [mobileNavigationActive, setMobileNavigationActive] = useState(false);
+  const [nameFieldValue, setNameFieldValue] = useState(
+    defaultState.current.nameFieldValue
+  );
+  const [emailFieldValue, setEmailFieldValue] = useState(
+    defaultState.current.emailFieldValue
+  );
+  const handleDiscard = useCallback(() => {
+    setEmailFieldValue(defaultState.current.emailFieldValue);
+    setNameFieldValue(defaultState.current.nameFieldValue);
+    setIsDirty(false);
+  }, []);
+  const handleSave = useCallback(() => {
+    defaultState.current.nameFieldValue = nameFieldValue;
+    defaultState.current.emailFieldValue = emailFieldValue;
+
+    setIsDirty(false);
+    setToastActive(true);
+  }, [emailFieldValue, nameFieldValue]);
+
+  const handleSearchResultsDismiss = useCallback(() => {
+    setSearchActive(false);
+    setSearchValue("");
+  }, []);
+  const handleSearchFieldChange = useCallback((value) => {
+    setSearchValue(value);
+    setSearchActive(value.length > 0);
+  }, []);
+  const toggleToastActive = useCallback(
+    () => setToastActive((toastActive) => !toastActive),
+    []
+  );
+  const toggleUserMenuActive = useCallback(
+    () => setUserMenuActive((userMenuActive) => !userMenuActive),
+    []
+  );
+  const toggleMobileNavigationActive = useCallback(
+    () =>
+      setMobileNavigationActive(
+        (mobileNavigationActive) => !mobileNavigationActive
+      ),
+    []
+  );
+  const toggleIsLoading = useCallback(
+    () => setIsLoading((isLoading) => !isLoading),
+    []
+  );
+
+  const toastMarkup = toastActive ? (
+    <Toast onDismiss={toggleToastActive} content="Changes saved" />
+  ) : null;
+
+  const userMenuActions = [
+    {
+      items: [{ content: "خروج" }],
+    },
+  ];
+
+  const contextualSaveBarMarkup = isDirty ? (
+    <ContextualSaveBar
+      message="Unsaved changes"
+      saveAction={{
+        onAction: handleSave,
+      }}
+      discardAction={{
+        onAction: handleDiscard,
+      }}
+    />
+  ) : null;
+
+  const userMenuMarkup = (
+    <TopBar.UserMenu
+      actions={userMenuActions}
+      name="کارجو +"
+      detail={"نگار رحمانی"}
+      initials="ن"
+      open={userMenuActive}
+      onToggle={toggleUserMenuActive}
+    />
+  );
+
+  const searchResultsMarkup = (
+    <ActionList
+      items={[
+        { content: "Shopify help center" },
+        { content: "Community forums" },
+      ]}
+    />
+  );
+
+  const searchFieldMarkup = (
+    <TopBar.SearchField
+      onChange={handleSearchFieldChange}
+      value={searchValue}
+      placeholder="Search"
+    />
+  );
+
+  const topBarMarkup = (
+    <TopBar
+      showNavigationToggle
+      userMenu={userMenuMarkup}
+      searchResultsVisible={searchActive}
+      searchField={searchFieldMarkup}
+      searchResults={searchResultsMarkup}
+      onSearchResultsDismiss={handleSearchResultsDismiss}
+      onNavigationToggle={toggleMobileNavigationActive}
+    />
+  );
+
+  const navigationMarkup = (
+    <Navigation location="/">
+      <Navigation.Section
+        items={[
+          {
+            label: "برگشت",
+            icon: ArrowLeftMinor,
+            onClick: () => push("/"),
+          },
+        ]}
+      />
+      <Navigation.Section
+        separator
+        items={[
+          {
+            label: "داشبورد",
+            icon: HomeMajor,
+            onClick: toggleIsLoading,
+          },
+          {
+            label: "تنظیمات",
+            icon: SettingsMajor,
+            onClick: toggleIsLoading,
+          },
+        ]}
+      />
+    </Navigation>
+  );
+
+  const loadingMarkup = isLoading ? <Loading /> : null;
+
+  const actualPageMarkup = (
+    <Page title={"اطلاعات شخصی"}>
+      <Layout>
+        <Card sectioned>
+          <DescriptionList
+            items={[
+              {
+                term: "نام",
+                description: "نگار ",
+              },
+              {
+                term: "نام خانوادگی",
+                description: "رحمانی",
+              },
+              {
+                term: "رشته تحصیلی",
+                description: "فناوری اطلاعات",
+              },
+            ]}
+          />
+        </Card>
+      </Layout>
+    </Page>
+  );
+
+  const loadingPageMarkup = (
+    <SkeletonPage>
+      <Layout>
+        <Layout.Section>
+          <Card sectioned>
+            <TextContainer>
+              <SkeletonDisplayText size="small" />
+              <SkeletonBodyText lines={9} />
+            </TextContainer>
+          </Card>
+        </Layout.Section>
+      </Layout>
+    </SkeletonPage>
+  );
+
+  const pageMarkup = isLoading ? loadingPageMarkup : actualPageMarkup;
+
+  return (
+    <div style={{ height: "500px", direction: "rtl" }}>
+      <AppProvider
+        i18n={{
+          Polaris: {
+            Avatar: {
+              label: "Avatar",
+              labelWithInitials: "Avatar with initials {initials}",
+            },
+            ContextualSaveBar: {
+              save: "Save",
+              discard: "Discard",
+            },
+            TextField: {
+              characterCount: "{count} characters",
+            },
+            TopBar: {
+              toggleMenuLabel: "Toggle menu",
+
+              SearchField: {
+                clearButtonLabel: "Clear",
+                search: "Search",
+              },
+            },
+            Modal: {
+              iFrameTitle: "body markup",
+            },
+            Frame: {
+              skipToContent: "Skip to content",
+              navigationLabel: "Navigation",
+              Navigation: {
+                closeMobileNavigationLabel: "Close navigation",
+              },
+            },
+          },
+        }}
+      >
+        <Frame
+          topBar={topBarMarkup}
+          navigation={navigationMarkup}
+          showMobileNavigation={mobileNavigationActive}
+          onNavigationDismiss={toggleMobileNavigationActive}
+          skipToContentTarget={
+            skipToContentRef?.current ? skipToContentRef?.current : undefined
+          }
+        >
+          {contextualSaveBarMarkup}
+          {loadingMarkup}
+          {pageMarkup}
+          {toastMarkup}
+        </Frame>
+      </AppProvider>
+    </div>
+  );
+}
